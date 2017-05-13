caef0056ecf38096b196e828a3933027a8615129
Pete D <peted70@hotmail.com>
2017-05-13 13:27:49 +0100

    added windowssoreapp folder to ignore file

---
+++ vrworld-demo/Assets/RabbitDance.cs
@@ -0,0 +1,10 @@
+﻿using HoloToolkit.Unity.InputModule;
+using UnityEngine;
+
+public class RabbitDance : MonoBehaviour, IFocusable
+{
+    public void OnFocusEnter()
+    {
+        gameObject.GetComponent<AudioSource>().Play();
+    }
+}
---
+++ vrworld-demo/Assets/RabbitScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RabbitScript : MonoBehaviour
+{
+    public void OnDrop()
+    {
+        // Remove the RigidBody component from the rabbit
+        var rabbit = GameObject.Find("Rabbit");
+        var rb = rabbit.GetComponent<Rigidbody>();
+        if (rb != null)
+            Destroy(rb);
+
+        rabbit.GetComponent<AudioSource>().Play();
+        rabbit.AddComponent<RabbitDance>();
+    }
+
+    public void OnMove()
+    {
+        var rabbit = GameObject.Find("Rabbit");
+        
+        var randomAngle = Random.Range(-45, 45);
+        gameObject.transform.localRotation =
+            Quaternion.Euler(0, randomAngle, 0);
+    }
+}
